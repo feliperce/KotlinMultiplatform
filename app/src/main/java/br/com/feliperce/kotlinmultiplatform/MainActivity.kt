205f6e40059f4d649a86674d4726d32595763205
Felipe <felipe.rce@gmail.com>
2020-04-01 23:37:31 -0300

    Created calculation common

--- app/src/main/java/br/com/feliperce/kotlinmultiplatform/MainActivity.kt
+++ app/src/main/java/br/com/feliperce/kotlinmultiplatform/MainActivity.kt
@@ -2,11 +2,18 @@ package br.com.feliperce.kotlinmultiplatform
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import getImc
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        calculateButton.setOnClickListener {
+            Toast.makeText(this, getImc(70, 1.70f).toString(), Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- shared/src/commonMain/kotlin/Calculation.kt
+++ shared/src/commonMain/kotlin/Calculation.kt
@@ -1,1 +1,30 @@
 
+fun getImc(weight: Int, height: Float): Float {
+    return weight/(height*height)
+}
+
+fun getImcResult(imc: Float): String {
+    return when {
+        imc <= 18.5 -> {
+            "Abaixo do peso"
+        }
+        imc in 18.6 .. 24.9 -> {
+            "Peso normal"
+        }
+        imc in 25.0 .. 29.9 -> {
+            "Sobrepeso"
+        }
+        imc in 30.0 .. 34.9 -> {
+            "Obesidade grau 1"
+        }
+        imc in 35.0 .. 39.9 -> {
+            "Obesidade grau 2"
+        }
+        imc >= 40 -> {
+            "Obesidade grau 3"
+        }
+        else -> {
+            "Alien"
+        }
+    }
+}
